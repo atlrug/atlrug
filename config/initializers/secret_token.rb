6a0827e98f932c5b24909efd842b405c82d561c3
Al Snow <jasnow@hotmail.com>
2013-04-24 16:08:37 -0400

    trying to fix secret env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Atlrug4::Application.config.secret_key_base = ENV["AR4_S_K_B"]
+Atlrug4::Application.config.secret_key_base = ENV['AR4_S_K_B']
