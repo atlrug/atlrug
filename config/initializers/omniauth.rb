661f9ac3f08a24d2d1832e3e5eb42553eedade66
Al Snow <jasnow@hotmail.com>
2013-05-13 20:17:01 -0400

    Changed provider line for omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "repo, user"
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
