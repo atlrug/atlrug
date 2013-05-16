a647c44d335efc72e64af03f6a207c2bdc50f023
Al Snow <jasnow@hotmail.com>
2013-05-16 12:53:34 -0400

    updated gems ; tried green_onion gem/broken

---
+++ doc/skinner.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+#require 'rubygems'
+require 'green_onion'
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+
+GreenOnion.configure do |c|
+  # You can use and customize these configuration options
+  # c.skins_dir = "#{::Rails.root}/spec/skins"
+  # c.threshold = 100
+  # c.dimensions = { :width => 1024, :height => 768 }
+  # c.skin_name = {
+  #   :match => /[\/]/, 
+  #   :replace => "_", 
+  #   :prefix => nil,
+  #   :root => "root" 
+  # }
+end
+
+all_routes = Rails.application.routes.routes
+routes = all_routes.collect { |r| r.path.spec.to_s.gsub(/\/*(\(\.)*:(\w*)(\))*\/*/, "") }.delete_if(&:empty?)
+
+routes.each do |route|
+  GreenOnion.skin_visual_and_percentage("http://localhost:3000" + route)
+end
