4dcd52ed9ceb539d159dcba66d5eb6b5fb8630e0
James Dabbs <jamesdabbs@gmail.com>
2014-06-11 19:46:08 -0400

    Save a Youtube hit
    We were calling the API with a playlist id to get the id of the playlist
    >_<

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,16 +3,23 @@ require 'youtube_it'
 class Video
 
   def self.all
-    client = YouTubeIt::Client.new(:dev_key => ENV['YOUTUBE_KEY'])
-
-    playlist = client.playlist(ENV['YOUTUBE_PLAYLIST_ID'])
     playlist.videos
   end
 
   def self.playlist_url
-    client = YouTubeIt::Client.new(:dev_key => ENV['YOUTUBE_KEY'])
+    "http://www.youtube.com/playlist?list=#{playlist_id}"
+  end
 
-    playlist = client.playlist(ENV['YOUTUBE_PLAYLIST_ID'])
-    "http://www.youtube.com/playlist?list=#{playlist.playlist_id}"
+  private
+
+  def self.playlist
+    @_playlist ||= begin
+      client = YouTubeIt::Client.new(:dev_key => ENV['YOUTUBE_KEY'])
+      client.playlist playlist_id
+    end
+  end
+
+  def self.playlist_id
+    ENV["YOUTUBE_PLAYLIST_ID"] || "PLqZY2tk6rSRn2Vapk2raOTFjXGiDbpQUU"
   end
 end
